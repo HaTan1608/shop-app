a4fafddf31e382dc9b87d2c9f439568bd487473b
HaTan1608 <63892386+HaTan1608@users.noreply.github.com>
2020-11-27 12:32:17 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc08e43b83a2f001f2e4081",
+				"5fc08e43b83a2f001f2e4083"
+			]
+		},
+		"5fc08e43b83a2f001f2e4081": {
+			"id": "5fc08e43b83a2f001f2e4081",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc08e43b83a2f001f2e4083": {
+			"id": "5fc08e43b83a2f001f2e4083",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
